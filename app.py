26dd51fdeedaeb18cd1850fb4e4b94eeec74c473
tobi <tobibags19@gmail.com>
2023-05-05 12:40:20 -0500

    Added a feedback interaction for when users click the submit button

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -32,6 +32,7 @@ def index():
                     email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        flash(f"{first_name}, your form was submitted successfully!", "success")
 
     return render_template("index.html")
 
