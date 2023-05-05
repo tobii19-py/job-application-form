e3638a71551be715c875217fea6993e858a33041
tobi <tobibags19@gmail.com>
2023-05-05 12:18:10 -0500

    Implement SQLite Database

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -15,4 +29,7 @@ def index():
     return render_template("index.html")
 
 
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
 app.run(debug=True, port=5001)
